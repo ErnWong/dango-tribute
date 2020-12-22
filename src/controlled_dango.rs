15fb77e65abd481867fe770eb3f2dc7f231f7955
Ernest Wong <sudo@ernestwong.nz>
2020-12-23 11:32:12 +1300

    Implement simple force control on blob

--- src/controlled_dango.rs
+++ src/controlled_dango.rs
@@ -1,6 +1,10 @@
 use super::physics::NPhysicsBodyHandleComponent;
 use bevy::prelude::*;
-use nphysics2d::{nalgebra::Vector2, object::DefaultBodySet};
+use nphysics2d::{
+    math::{Force, ForceType},
+    nalgebra::Vector2,
+    object::DefaultBodySet,
+};
 
 use super::RealField;
 
@@ -12,20 +16,27 @@ pub fn controlled_dango_system(
     query: Query<(&ControlledDangoComponent, &NPhysicsBodyHandleComponent)>,
 ) {
     for (_, body_handle) in &mut query.iter() {
-        if let Some(body) = bodies.rigid_body_mut(body_handle.handle()) {
+        if let Some(body) = bodies.get_mut(body_handle.handle()) {
             let horizontal_movement = ((input.pressed(KeyCode::D) || input.pressed(KeyCode::Right))
                 as i32
                 - (input.pressed(KeyCode::A) || input.pressed(KeyCode::Left)) as i32)
                 as f32;
-            body.set_linear_velocity(
-                body.velocity().linear + Vector2::new(horizontal_movement * 10.0, 0.0),
+            body.apply_force(
+                0,
+                &Force::linear(Vector2::x() * horizontal_movement * 7.0),
+                ForceType::Force,
+                false,
             );
             if input.just_pressed(KeyCode::W)
                 || input.just_pressed(KeyCode::Space)
                 || input.just_pressed(KeyCode::Up)
             {
-                body.set_linear_velocity(
-                    body.velocity().linear.component_mul(&Vector2::x()) + Vector2::new(0.0, 200.0),
+                info!("Jump - (this log is to troubleshoot unresponsive states)");
+                body.apply_force(
+                    0,
+                    &Force::linear(Vector2::y() * 7.0),
+                    ForceType::Impulse,
+                    false,
                 );
             }
         }
