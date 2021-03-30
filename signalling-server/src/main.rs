734b34f6d6d65e888bd340cd8910c8fff4a87156
Ernest Wong <sudo@ernestwong.nz>
2021-03-30 14:37:50 +1300

    Support multiple hosts and return host id to host

--- signalling-server/src/main.rs
+++ signalling-server/src/main.rs
@@ -1,44 +1,97 @@
 use actix::prelude::*;
 use actix_cors::Cors;
 use actix_web::{
-    error, get,
-    http::{header, StatusCode},
-    post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+    get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 use actix_web_actors::ws;
-use std::{
-    collections::{HashMap, VecDeque},
-    sync::Mutex,
-};
+use std::collections::{HashMap, VecDeque};
 use tokio::sync::oneshot;
 
 mod test;
 
 #[derive(Default, Debug)]
+struct RoomRegistry {
+    rooms: HashMap<String, Addr<RoomHost>>,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct HostConnect(String, Addr<RoomHost>);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct HostDisconnect(String);
+
+#[derive(Message)]
+#[rtype(result = "Option<Addr<RoomHost>>")]
+struct GetHost(String);
+
+impl Actor for RoomRegistry {
+    type Context = Context<Self>;
+}
+
+impl Handler<HostConnect> for RoomRegistry {
+    type Result = ();
+    fn handle(&mut self, msg: HostConnect, ctx: &mut Self::Context) {
+        let HostConnect(id, host_address) = msg;
+        self.rooms.insert(id, host_address);
+    }
+}
+
+impl Handler<HostDisconnect> for RoomRegistry {
+    type Result = ();
+    fn handle(&mut self, msg: HostDisconnect, ctx: &mut Self::Context) {
+        let HostDisconnect(id) = msg;
+        self.rooms.remove(&id);
+    }
+}
+
+impl Handler<GetHost> for RoomRegistry {
+    type Result = Option<Addr<RoomHost>>;
+    fn handle(&mut self, msg: GetHost, ctx: &mut Self::Context) -> Self::Result {
+        let GetHost(id) = msg;
+        self.rooms.get(&id).cloned()
+    }
+}
+
+#[derive(Debug)]
 struct RoomHost {
     answer_queue: VecDeque<oneshot::Sender<String>>,
+    id: String,
+    registry_address: Addr<RoomRegistry>,
 }
 
 #[derive(Message)]
 #[rtype(result = "Result<String, ()>")]
 struct Offer(String);
 
-// impl Message for Offer {
-//     // type Result = ResponseFuture<()>;
-//     type Result = ResponseFuture<Result<(), ()>>;
-//     //type Result = ResponseFuture<Result<String, ()>>;
-// }
+impl RoomHost {
+    fn new(registry_address: Addr<RoomRegistry>) -> RoomHost {
+        RoomHost {
+            answer_queue: Default::default(),
+            id: nanoid::simple(),
+            registry_address,
+        }
+    }
+}
 
 impl Actor for RoomHost {
     type Context = ws::WebsocketContext<Self>;
 
-    fn started(&mut self, _ctx: &mut Self::Context) {
+    fn started(&mut self, ctx: &mut Self::Context) {
+        println!("Registering new room...");
+        self.registry_address
+            .do_send(HostConnect(self.id.clone(), ctx.address()));
+        // TODO: error handling
         println!("Room opened");
+        ctx.text(self.id.clone());
     }
 
     fn stopped(&mut self, _ctx: &mut Self::Context) {
+        println!("Deregistering room...");
+        self.registry_address
+            .do_send(HostDisconnect(self.id.clone()));
         println!("Room closed");
-        // TODO: Remove from map
     }
 }
 
@@ -69,45 +122,42 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for RoomHost {
 async fn join(
     req: HttpRequest,
     offer: String,
-    room_hosts: web::Data<Mutex<HashMap<String, Addr<RoomHost>>>>,
+    room_registry_address: web::Data<Addr<RoomRegistry>>,
 ) -> impl Responder {
-    let id = req.match_info().query("id");
-    println!("Joining id: {:?}", id);
-    println!("room_hosts = {:?}", room_hosts);
-    let room_hosts_locked = room_hosts.lock().unwrap();
-    if let Some(room_host) = room_hosts_locked.get(id) {
-        let answer = room_host.send(Offer(offer)).await;
+    let id = req.match_info().query("id").to_string();
+    match room_registry_address.send(GetHost(id)).await {
+        Ok(Some(room_host)) => {
+            let answer = room_host.send(Offer(offer)).await;
 
-        match answer {
-            Err(_) => HttpResponse::InternalServerError().body("no"), // TODO
-            Ok(Err(_)) => HttpResponse::InternalServerError().body("no"), // TODO
-            Ok(Ok(answer_text)) => HttpResponse::Ok().body(answer_text),
+            match answer {
+                Err(_) => HttpResponse::InternalServerError().body(""), // TODO
+                Ok(Err(_)) => HttpResponse::InternalServerError().body(""), // TODO
+                Ok(Ok(answer_text)) => HttpResponse::Ok().body(answer_text),
+            }
         }
-    } else {
-        HttpResponse::NotFound().body("Room not found")
+        Ok(None) => HttpResponse::NotFound().body("Room with given id not found"),
+        Err(_) => HttpResponse::InternalServerError().body(""), // TODO
     }
-    //.map_err(|error| error::InternalError::new(error, StatusCode::INTERNAL_SERVER_ERROR))?;
-    //answer.map_err(|error| error::InternalError::new(error, StatusCode::INTERNAL_SERVER_ERROR))
 }
 
 #[get("/host")]
 async fn host(
     req: HttpRequest,
     stream: web::Payload,
-    room_hosts: web::Data<Mutex<HashMap<String, Addr<RoomHost>>>>,
+    room_registry_address: web::Data<Addr<RoomRegistry>>,
 ) -> Result<HttpResponse, Error> {
-    let (addr, resp) = ws::start_with_addr(RoomHost::default(), &req, stream)?;
-    //let id = nanoid::simple(); // TODO
-    let id = "1234".to_string();
-    println!("Hosting id: {:?}", id);
-    room_hosts.lock().unwrap().insert(id, addr);
-    println!("room_hosts = {:?}", room_hosts);
+    println!("Starting new host actor...");
+    let resp = ws::start(
+        RoomHost::new(room_registry_address.get_ref().clone()),
+        &req,
+        stream,
+    )?;
     Ok(resp)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let room_hosts = web::Data::new(Mutex::new(HashMap::<String, Addr<RoomHost>>::new()));
+    let room_registry_address = RoomRegistry::default().start();
     HttpServer::new(move || {
         App::new()
             .wrap(
@@ -116,7 +166,7 @@ async fn main() -> std::io::Result<()> {
                     .allow_any_method()
                     .allow_any_header(),
             )
-            .app_data(room_hosts.clone())
+            .data(room_registry_address.clone())
             .service(join)
             .service(host)
     })
