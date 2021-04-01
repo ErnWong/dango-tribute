8f119a6d407f00d9025a31f516d91614da6455ab
Ernest Wong <sudo@ernestwong.nz>
2021-04-01 14:49:03 +1300

    Bump physics resolution back up to prevent entanglement

--- shared/src/settings.rs
+++ shared/src/settings.rs
@@ -1,6 +1,6 @@
 use bevy_prototype_networked_physics::Config as NetworkedPhysicsConfig;
 
-pub const TIMESTEP: f32 = 1.0 / 30.0;
+pub const TIMESTEP: f32 = 1.0 / 60.0;
 pub const GRAVITY: f32 = -9.81 * 1.5;
 pub type RealField = f32;
 pub const NETWORKED_PHYSICS_CONFIG: NetworkedPhysicsConfig = NetworkedPhysicsConfig {
